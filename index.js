d0bcad65b0ee1b5b844de4bb40ff450facbf2c38
Jason Hitching <jasonhitching1994@gmail.com>
2020-06-29 20:59:19 +0100

    Delivery page added, content page updated with the beginning of a contact form and a placeholder map image of the company location

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(".carousel").carousel({
   interval: 3000,
 });
+
+var map;
+function initialize() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(37.4419, -122.1419),
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  });
+}
+google.maps.event.addDomListener(window, "load", initialize);
